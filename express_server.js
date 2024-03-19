9e239f4b07ac03d18a65eba97345f169587ba445
Dicky jr <lmayhenderson@yahoo.com>
2024-03-18 20:07:54 -0700

    We added a new form page (urls_new.js), a new get route for this file and app.use in express to parse the body

--- express_server.js
+++ express_server.js
@@ -4,6 +4,8 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -30,7 +32,12 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
+
