2e9beb9d13ab55b2f14c452ac43b8ca2d59a5fd6
Dicky jr <lmayhenderson@yahoo.com>
2024-03-19 18:49:50 -0700

    added form to Urls_index to that submits sn udpated longUrl with accompanying button + get & post routes

--- express_server.js
+++ express_server.js
@@ -9,22 +9,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use((err, req, res, next) => {
   console.error(err.stack);
   //send status code telling us to get Bob the Builder on it!
-  res.status(500).send("WARNING\nsomething's broken")
-})
+  res.status(500).send("WARNING\nsomething's broken");
+});
 
 const generateRandomString = function() {
-//variable to store randomly generated string
+  //variable to store randomly generated string
   let newStr = '';
   //store alphabet + numbers 1-9 to loop over/use method
-  const alphaNum = 'abcdefghijklmnopqrstuvwxyz123456789'
+  const alphaNum = 'abcdefghijklmnopqrstuvwxyz123456789';
   //loop over alphaNum 6 times to generate 6 different letters/nums
   for (let i = 0; i < 6; i++) {
-  const randomStr = (Math.floor(Math.random() * alphaNum.length))
-  newStr += alphaNum[randomStr]
+    const randomStr = (Math.floor(Math.random() * alphaNum.length));
+    newStr += alphaNum[randomStr];
   }
-//OUTPUT: a string of 6 random alpha-numeric charachters
+  //OUTPUT: a string of 6 random alpha-numeric charachters
   return newStr;
-}
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -32,12 +32,12 @@ const urlDatabase = {
 };
 
 app.get('/', (req, res) => {
-	res.send("Hello!")
-})
+  res.send("Hello!");
+});
 
 app.listen(PORT, () => {
-	console.log(`Example app listening on port ${PORT}!`);
-})
+  console.log(`Example app listening on port ${PORT}!`);
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -57,45 +57,64 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
 //route handler for POST requests to /url
 app.post("/urls", (req, res) => {
   try {
-  //get id-longURL key + values
-  const id = generateRandomString()
-  const longURL = req.body.longURL;
+    //get id-longURL key + values
+    const id = generateRandomString();
+    const longURL = req.body.longURL;
 
-  //store the key + value in the urlDatabase
-  urlDatabase[id] = longURL;
-//redirect users to /urls/:id
-  res.redirect(`/urls/${id}`)
+    //store the key + value in the urlDatabase
+    urlDatabase[id] = longURL;
+    //redirect users to /urls/:id
+    res.redirect(`/urls/${id}`);
   }
   catch (error) {
-    console.log(error.message)
-    res.status(500).send('Server error')
+    console.log(error.message);
+    res.status(500).send('Server error');
   }
-})
+});
 
 //Route handler to handle shortURL requests
 app.get("/u/:id", (req, res) => {
   try {
-  //request end point "/u/:id"
-  const longURL = urlDatabase[req.params.id]
-  //redirect to its longURL
-  res.redirect(longURL);
+    //request end point "/u/:id"
+    const longURL = urlDatabase[req.params.id];
+    //redirect to its longURL
+    res.redirect(longURL);
   } catch (error) {
-    console.log(error.message)
-    res.status(500).send('Server error')
+    console.log(error.message);
+    res.status(500).send('Server error');
   }
 });
 
 //adding a post route that removes url resource: '/urls/:id/delete'
 app.post("/urls/:id/delete", (req, res) => {
-  const id = req.params.id
-  delete urlDatabase[id]
+  const id = req.params.id;
+  delete urlDatabase[id];
   //redirect client to /urls
+  res.redirect('/urls');
+});
+
+//get route to get the edit form for a specific url
+app.get("/urls/:id/editor", (req, res) => {
+  const id = req.params.id
+  const longUrl = urlDatabase[id]
+
+  res.render("edit_form", {id: id, longUrl: longUrl})
+})
+
+//new post route to update the value of the stored long URL based on the new value in req.body
+app.post("/urls/:id/edit", (req, res) => {
+  const id = req.params.id
+  const newLongUrl = req.body.Url
+ 
+  urlDatabase[id] = newLongUrl
+  //redirect the client back to urls
   res.redirect('/urls')
 })
+
